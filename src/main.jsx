34b0ce9021a6cdb3dcba47ad36219210082de1e0
Giphy2 <152769739+Giphy2@users.noreply.github.com>
2025-10-29 10:48:13 +0300

    Updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from '../frontend/src/App.jsx';
+import '../frontend/src/styles/theme.css';
 import './index.css';
 
 const rootElement = document.getElementById('root');
